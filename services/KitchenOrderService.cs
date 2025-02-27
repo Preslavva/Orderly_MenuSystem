43f80a792a0ee7c6ab9b1cb92abcd788f8b95c1f
Joshua Sanchez Ruiz <jjsanchez-ruiz@hotmail.com>
2025-02-27 12:59:45 +0100

    added kitchenOrder servcies

---
+++ services/KitchenOrderService.cs
@@ -0,0 +1,30 @@
+﻿using OrderlyTest.Models;
+using OrderlyTest.repos;
+
+namespace OrderlyTest.services
+{
+    public class KitchenOrderService
+    {
+
+        private readonly OrderRepositories _orderRepositories;
+        public KitchenOrderService(OrderRepositories orderRepository)
+        {
+            _orderRepositories = orderRepository;   
+        }
+
+        public List<Order> GetOrderByStatus(OrderStatus status)
+        {
+           return _orderRepositories.GetOrdersByStatus(status);
+        }
+
+        public Order GetOrderById(int orderId)
+        {
+            return _orderRepositories.GetOrderById(orderId);
+        }
+
+        public void  UpdateOrderStatus(int orderId, OrderStatus orderStatus)
+        {
+            _orderRepositories.UpdateOrderStatus(orderId, orderStatus);
+        }
+    }
+}
