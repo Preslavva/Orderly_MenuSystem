96724999357881d738b213208c7267b5295c030e
KaterinaSSlavova <k.slavova@student.fontys.nl>
2025-02-27 09:34:40 +0100

    added table entities

--- Models/Table.cs
+++ Models/Table.cs
@@ -2,7 +2,19 @@
 {
     public class Table
     {
-		
+        public int Id { get; }
+        public string QrCode { get; }
 
-	}
+        public Table(int id, string qrCode)
+        {
+            this.Id = id;
+            this.QrCode = qrCode;
+        }
+
+        public Table(string qrCode)
+        {
+            this.QrCode=qrCode;
+        }
+
+    }
 }
