fe15ec06016ef6465136e0d9b979c5e8ff5eba99
KaterinaSSlavova <k.slavova@student.fontys.nl>
2025-02-27 09:56:03 +0100

    adding connection to the database

---
+++ repos/DbHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace OrderlyTest.repos
+{
+    public class DbHelper
+    {
+        private const string connectionString = "Server=mssqlstud.fhict.local;Database=dbi547761;User Id=dbi547761s;Password=12345; Encrypt=True;TrustServerCertificate=True;";
+    }
+}
