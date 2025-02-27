9672c076baff8ed7db40070b12f7e42e0f2016d5
PGocheva <p.gocheva@student.fontys.nl>
2025-02-27 09:35:49 +0100

    created class MenuItem

--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -1,6 +1,34 @@
-﻿namespace OrderlyTest.Models
+﻿using System.Net.Http.Headers;
+
+namespace OrderlyTest.Models
 {
     public class MenuItem
     {
+        public int Id { get; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        bool IsAvailable { get; set; }
+
+        public MenuItem(string name, string description, decimal price, bool isAvailable)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+            this.IsAvailable = IsAvailable;
+        }
+
+        public MenuItem(int id,string name, string description, decimal price, bool isAvailable)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+            this.IsAvailable = IsAvailable;
+        }
     }
 }
