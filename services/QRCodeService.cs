2a156b4a04d70d14701be5eab0647e1545dd8258
Mirela28 <mirelagirleanu@gmail.com>
2025-02-27 10:57:57 +0100

    QrService

---
+++ services/QRCodeService.cs
@@ -0,0 +1,19 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.IO;
+
+namespace OrderlyTest.services
+{
+    public class QRCodeService
+    {
+        public byte[] GenerateQRCode(string url)
+        {
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())  // QR Code generator
+            {
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q); // Create QR code data
+                PngByteQRCode qrCode = new PngByteQRCode(qrCodeData); // Generates QR as PNG bytes
+                return qrCode.GetGraphic(20); // Returns the PNG byte array
+            }
+        }
+    }
+}
