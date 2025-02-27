2c9a5989e5fec16cb243736405844dacddf6826f
Joshua Sanchez Ruiz <jjsanchez-ruiz@hotmail.com>
2025-02-27 10:38:09 +0100

    added kitchenOrder class

---
+++ Models/KitchenOrderManager.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderlyTest.Models
+{
+    public class KitchenOrderManager
+    {
+        public List<Order> pendingOrders { get; set; }
+        public List<Order> completedOrders { get; set; }
+
+        public KitchenOrderManager()
+        {
+            pendingOrders = new List<Order>();
+            completedOrders = new List<Order>();
+        }
+    }
+}
