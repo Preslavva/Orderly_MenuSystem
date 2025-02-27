b8f40486fa8189295b021a2a2ea7d071da86b938
Mirela28 <mirelagirleanu@gmail.com>
2025-02-27 11:50:41 +0100

    qr controller

---
+++ Controllers/QRController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderlyTest.services;
+
+namespace OrderlyTest.Controllers
+{
+    public class QRController : Controller
+    {
+        private readonly QRCodeService _qrCodeService;
+
+        public QRController()
+        {
+            _qrCodeService = new QRCodeService();
+        }
+
+        public IActionResult Generate(int tableId)
+        {
+            string url = $"{Request.Scheme}://{Request.Host}/Order/Menu?tableId={tableId}";
+            byte[] qrCodeImage = _qrCodeService.GenerateQRCode(url); 
+            return File(qrCodeImage, "image/png"); 
+        }
+    }
+}
