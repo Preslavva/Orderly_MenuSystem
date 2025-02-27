3130f543051e254dfe3d58f5467a13a98d3fac8c
KaterinaSSlavova <k.slavova@student.fontys.nl>
2025-02-27 09:45:56 +0100

    added order entities

--- Models/Order.cs
+++ Models/Order.cs
@@ -2,5 +2,27 @@
 {
     public class Order
     {
+        public int Id { get; }
+        public  Table  Table { get; set; }
+        public DateTime OrderTimestamp { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; }
+
+        public Order(int id, Table table, DateTime orderTimestamp, OrderStatus status)
+        {
+            this.Id = id;
+            this.Table = table;
+            this.OrderTimestamp = orderTimestamp;
+            this.Status = status;
+            this.Items= new List<OrderItem>();
+        }
+
+        public Order(Table table, DateTime orderTimestamp, OrderStatus status)
+        {
+            this.Table = table;
+            this.OrderTimestamp = orderTimestamp;
+            this.Status = status;
+            this.Items = new List<OrderItem>();
+        }
     }
 }
