868dac3cc567e5eeddf0e125a988885cdfea4f82
Joshua Sanchez Ruiz <jjsanchez-ruiz@hotmail.com>
2025-03-27 14:42:45 +0100

    added a niew ViewModel called KitchenOrderManagerViewModel

---
+++ MainOrderly/ViewModels/KitchenOrderManagerViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Models.Entities;
+
+namespace MainOrderly.WebApp.ViewModels
+{
+    public class KitchenOrderManagerViewModel
+    {
+        public List<Order> NewOrders { get; set; }
+        public List<Order> PendingOrders { get; set; }
+        public List<Order> CompletedOrders { get; set; }
+
+        public KitchenOrderManagerViewModel()
+        {
+            this.NewOrders = new List<Order>();
+            this.PendingOrders = new List<Order>();
+            this.CompletedOrders = new List<Order>();
+        }
+
+
+        public static KitchenOrderManagerViewModel FromEntity(KitchenOrderManager kitchenOrderManager)
+        {
+            return new KitchenOrderManagerViewModel
+            {
+                NewOrders = kitchenOrderManager.NewOrders,
+                PendingOrders = kitchenOrderManager.PendingOrders,
+                CompletedOrders = kitchenOrderManager.CompletedOrders
+            };
+        }
+
+
+
+    }
+}
--- Models/Entities/KitchenOrderManager.cs
+++ Models/Entities/KitchenOrderManager.cs
@@ -2,15 +2,30 @@
 {
     public class KitchenOrderManager
     {
-        public List<Order> NewOrders { get; set; }
-        public List<Order> PendingOrders { get; set; }
-        public List<Order> CompletedOrders { get; set; }
+        public List<Order> NewOrders { get; private set; }
+        public List<Order> PendingOrders { get; private set; }
+        public List<Order> CompletedOrders { get; private set; }
 
         public KitchenOrderManager()
         {
             NewOrders = new List<Order>();
             PendingOrders = new List<Order>();
             CompletedOrders = new List<Order>();
+        }
+
+        public void SetNewOrders(List<Order> newOrders)
+        {
+            NewOrders = newOrders;
+        }
+
+        public void SetPendingOrders(List<Order> pendingOrders)
+        {
+            PendingOrders = pendingOrders;
+        }
+
+        public void SetCompletedOrders(List<Order> completedOrders)
+        {
+            CompletedOrders = completedOrders;
         }
     }
 }
