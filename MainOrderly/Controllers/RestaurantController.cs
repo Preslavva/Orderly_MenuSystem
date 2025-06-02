760fd3fb2cb7b718cbb642bfdbe25dd3cfac1832
KaterinaSSlavova <k.slavova@student.fontys.nl>
2025-06-02 16:12:55 +0200

    adjusting customize view

--- MainOrderly/Controllers/RestaurantController.cs
+++ MainOrderly/Controllers/RestaurantController.cs
@@ -113,7 +113,7 @@ namespace MainOrderly.WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult UpdateRestaurantK(RestaurantViewModel restaurantViewModel)
+        public IActionResult CustomizeRestaurant(RestaurantViewModel restaurantViewModel)
         {
             try
             {
@@ -122,7 +122,7 @@ namespace MainOrderly.WebApp.Controllers
                     restaurantViewModel.Logo = _restaurantService.ConvertToString(restaurantViewModel.LogoImage);
                 }
                 Restaurant restaurant = RestaurantViewModel.ConvertToEntity(restaurantViewModel);
-                _restaurantService.UpdateRepository(restaurant);
+                _restaurantService.CustomizeRestaurant(restaurant);
                 TempData["SuccessMessage"] = "Restaurant was successfully updated!";
             }
             catch (ArgumentException ex)
@@ -130,13 +130,6 @@ namespace MainOrderly.WebApp.Controllers
                 TempData["ErrorMessage"] = ex.Message;
             }
             return RedirectToAction("CustomizeRestaurant", "Restaurant");
-        }
-
-        [HttpPost]
-        public IActionResult ArchiveRestaurantk(int id)
-        {
-            _restaurantService.RemoveRestaurant(id);
-            return RedirectToAction("CreateRestaurant", "Restaurant");
         }
 
         [HttpPost]
